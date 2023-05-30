a59d7a5d7b5d8fe231e604a902b0f1e6fb4c93c9
kimsoar <kimsoar17@gmail.com>
2023-05-30 19:21:21 +0900

    파라메타를 통한 값 전달

--- src/router.js
+++ src/router.js
@@ -4,9 +4,11 @@ import Home from './views/Home.vue'
 
 Vue.use(Router)
 
-const About = () => {
-  return import(/* webpackChunkName: "about" */ './views/About.vue')
-}
+const About = () => import(/* webpackChunkName: "about" */ './views/About.vue')
+
+const Users = () => 
+  import(/* webpackChunkName: "users" */ './views/Users.vue')
+
 
 export default new Router({
   mode: 'history',
@@ -24,6 +26,14 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: About
+    },
+    {
+      path: '/users/:userId',
+      name: 'users',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: Users
     }
   ]
 })
---
+++ src/views/Users.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="text-xs-center">
+    <h1>Users</h1>
+    <p>이 유저는 현재 유저 번호가 {{ userId }} 입니다. </p>
+    {{ $route.params.name }}
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    userId() {
+    
+      return this.$route.params.userId
+    }
+  },
+  created() {
+    console.log('router', this.$router);
+
+    console.log('route', this.$route);
+  }
+
+}
+</script>
